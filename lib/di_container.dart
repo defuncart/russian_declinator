4b76cc4b7356fe6e08eb328559cab5e5dcbf7993
James Leahy <defuncart@gmail.com>
2021-07-31 11:19:47 +0200

    Add database to di container.

--- lib/di_container.dart
+++ lib/di_container.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/widgets.dart' show Locale;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:russian_declinator/services/databases/database.dart';
 import 'package:russian_declinator/services/settings_database/i_settings_database.dart';
 import 'package:russian_declinator/services/settings_database/settings_database.dart';
+
+export 'package:russian_declinator/services/databases/database.dart';
+export 'package:russian_declinator/services/settings_database/i_settings_database.dart';
 
 abstract class DIContainer {
   static late Reader _read;
@@ -9,10 +13,16 @@ abstract class DIContainer {
   static void initialize(Reader read) => _read = read;
 
   static ISettingsDatabase get settingsDatabase => _read(_settingsProvider);
+
+  static Database get db => _read(_dbProvider);
 }
 
 final _settingsProvider = Provider<ISettingsDatabase>(
   (_) => SettingsDatabase(),
+);
+
+final _dbProvider = Provider<Database>(
+  (_) => Database(),
 );
 
 final isDarkModeProvider = StateProvider((ref) => DIContainer.settingsDatabase.isDarkMode);
