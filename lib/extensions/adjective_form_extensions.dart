7394e9b01f81eeb06b6a0907409cd0dcf359a637
James Leahy <defuncart@gmail.com>
2021-08-04 15:49:24 +0200

    Add AdjectiveFormExtensions

---
+++ lib/extensions/adjective_form_extensions.dart
@@ -0,0 +1,17 @@
+import 'package:russian_declinator/enums/adjective_form.dart';
+import 'package:russian_declinator/generated/l10n.dart';
+
+extension AdjectiveFormExtensions on AdjectiveForm {
+  String get localized {
+    switch (this) {
+      case AdjectiveForm.masc:
+        return AppLocalizations.current.generalMasculine;
+      case AdjectiveForm.fem:
+        return AppLocalizations.current.generalFeminine;
+      case AdjectiveForm.neu:
+        return AppLocalizations.current.generalNeuter;
+      case AdjectiveForm.plural:
+        return AppLocalizations.current.generalPlural;
+    }
+  }
+}
