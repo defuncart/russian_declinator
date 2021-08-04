1479794be517a0ba7699cd6e8baafda6b42c1ed5
James Leahy <defuncart@gmail.com>
2021-08-04 15:25:20 +0200

    NounExtensions deal with singular/plural only.

--- lib/extensions/noun_extensions.dart
+++ lib/extensions/noun_extensions.dart
@@ -2,17 +2,21 @@ import 'package:russian_declinator/services/databases/database.dart' show Noun;
 
 extension NounExtensions on Noun {
   List<String> get allDeclensions => [
-        sgNom,
-        sgGen,
-        sgDat,
-        sgAcc,
-        sgInst,
-        sgPrep,
-        plNom,
-        plGen,
-        plDat,
-        plAcc,
-        plInst,
-        plPrep,
+        if (!isPlural) ...[
+          sgNom,
+          sgGen,
+          sgDat,
+          sgAcc,
+          sgInst,
+          sgPrep,
+        ],
+        if (!isSingular) ...[
+          plNom,
+          plGen,
+          plDat,
+          plAcc,
+          plInst,
+          plPrep,
+        ]
       ];
 }
--- test/extensions/noun_extension_test.dart
+++ test/extensions/noun_extension_test.dart
@@ -6,11 +6,10 @@ import '../test_utils/test_instances.dart';
 void main() {
   group('NounExtensions', () {
     group('allDeclensions', () {
-      test('when noun contains all declensions, expect all declensions', () {
+      test('when noun is not singular or plural only, expect all declensions', () {
         final noun = TestInstances.noun();
-        final declensions = noun.allDeclensions;
         expect(
-          declensions,
+          noun.allDeclensions,
           [
             'sgNom',
             'sgGen',
@@ -18,6 +17,52 @@ void main() {
             'sgAcc',
             'sgInst',
             'sgPrep',
+            'plNom',
+            'plGen',
+            'plDat',
+            'plAcc',
+            'plInst',
+            'plPrep',
+          ],
+        );
+      });
+
+      test('when noun is singular only, expect only singular declensions', () {
+        final noun = TestInstances.noun(
+          isSingular: true,
+          plNom: '',
+          plGen: '',
+          plDat: '',
+          plAcc: '',
+          plInst: '',
+          plPrep: '',
+        );
+        expect(
+          noun.allDeclensions,
+          [
+            'sgNom',
+            'sgGen',
+            'sgDat',
+            'sgAcc',
+            'sgInst',
+            'sgPrep',
+          ],
+        );
+      });
+
+      test('when noun is plural only, expect only plural declensions', () {
+        final noun = TestInstances.noun(
+          isPlural: true,
+          sgNom: '',
+          sgGen: '',
+          sgDat: '',
+          sgAcc: '',
+          sgInst: '',
+          sgPrep: '',
+        );
+        expect(
+          noun.allDeclensions,
+          [
             'plNom',
             'plGen',
             'plDat',
